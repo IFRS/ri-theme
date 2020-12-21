4a537cdcc2a080971bf577bce08477db48a8cc54
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2020-12-21 17:21:10 -0300

    Ajuste nos atributos da barra brasil

--- src/script.js
+++ src/script.js
@@ -11,12 +11,11 @@ $(function() {
     $('[data-toggle="tooltip"]').tooltip();
 
     let barra = document.createElement('div');
-    barra.setAttribute('id', 'barra-brasil');
+    barra.id = 'barra-brasil';
     document.body.prepend(barra);
 
     var script = document.createElement('script');
     script.src = 'https://barra.brasil.gov.br/barra.js';
-    script.defer = true;
-
+    script.defer = 'defer';
     document.body.appendChild(script);
 });
