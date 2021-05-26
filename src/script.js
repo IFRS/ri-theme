ea579484a46045f37da70c7599a499b63ef4c29e
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2021-05-26 17:21:47 -0300

    Adição de link para o conteúdo

--- src/script.js
+++ src/script.js
@@ -14,8 +14,14 @@ $(function() {
     barra.id = 'barra-brasil';
     document.body.prepend(barra);
 
-    var script = document.createElement('script');
+    let script = document.createElement('script');
     script.src = 'https://barra.brasil.gov.br/barra.js';
     script.defer = 'defer';
     document.body.appendChild(script);
+
+    let jump = document.createElement('a');
+    jump.href = '#ds-body';
+    jump.text = 'Pular para o conteúdo';
+    jump.classList.add('sr-only', 'sr-only-focusable')
+    document.body.prepend(jump);
 });
